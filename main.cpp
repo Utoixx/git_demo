c8811b6618086e709a35820f66e649828017de62
tuanduong <quoctuan2000bg@gmail.com>
2024-11-14 23:30:31 +0700

    add new feature

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main(int argn, char* argc[]){
 	printf("hello world!\n");
 	//This line written by another dev
 	//this line written by me	
-	//this is new feature
+	//this is a new feature
 	
 	return 0;
 }
